b1837e57de5ef93e635486aa3b38b368b63950ee
Rozimuhammad05 <roziabdusamadov2005.10@gmail.com>
2024-01-27 10:57:27 +0500

    homework

---
+++ main.js
@@ -0,0 +1,7 @@
+var userName=prompt("Ismingizni Kiriting");
+var userAge=prompt("Yoshingizni kiriting");
+var userSurname=prompt("Familiyangizni kiriting");
+var userEmail=prompt("Emailingizni kiriting");
+var userMoney=prompt("Ayirboshlaydigan summani kiriting");
+var DOLLAR_UZS=12400;
+console.log( userName, "\n", userAge, "\n", userSurname, "\n", userEmail, "\n",  userMoney*DOLLAR_UZS);
